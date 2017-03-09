d03db5084fa0049e022025670da538e12b423252
tomusborne <tom@edge22.com>
2017-03-09 15:58:23 -0800

    WP coding standards

--- inc/functions.php
+++ inc/functions.php
@@ -223,8 +223,9 @@ function wpsp_hex2rgba($color, $opacity = false) {
 	$default = 'rgb(0,0,0)';
  
 	// Return default if no color provided
-	if(empty($color))
+	if ( empty( $color ) ) {
           return $default; 
+	}
  
 	// Sanitize $color if "#" is provided 
 	if ($color[0] == '#' ) {
@@ -232,21 +233,22 @@ function wpsp_hex2rgba($color, $opacity = false) {
 	}
 
 	// Check if color has 6 or 3 characters and get values
-	if (strlen($color) == 6) {
-			$hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
+	if ( strlen( $color ) == 6) {
+		$hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
 	} elseif ( strlen( $color ) == 3 ) {
-			$hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
+		$hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
 	} else {
-			return $default;
+		return $default;
 	}
 
 	// Convert hexadec to rgb
 	$rgb =  array_map('hexdec', $hex);
 
 	// Check if opacity is set(rgba or rgb)
-	if($opacity){
-		if(abs($opacity) > 1)
+	if ( $opacity ){
+		if( abs( $opacity ) > 1) {
 			$opacity = 1.0;
+		}
 		$output = 'rgba('.implode(",",$rgb).','.$opacity.')';
 	} else {
 		$output = 'rgb('.implode(",",$rgb).')';
